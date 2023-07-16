ca4aebf1a1b8d9a19dc4a682dd321bdcaeaaf5a8
abhiHunts <abhishek626pawar@gmail.com>
2023-07-16 16:53:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 import streamlit as st
 import torch
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 # Load the T5 model and tokenizer
-model_name = "t5-base-medium-title-generation/checkpoint-1200"
-model_dir = f"./checkpoint-1200-20230716T083021Z-001"
-
-tokenizer = AutoTokenizer.from_pretrained(model_dir)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
-
-
+model_name = 'abhi-pwr/news-summarizer'
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to('cpu')
+model.to(device)
 
 # Set maximum sequence length for the input text
 max_length = 512
@@ -22,9 +18,8 @@ def generate_summary(article):
     inputs = inputs.to(device)
 
     # Generate summary
-    output = model.generate(inputs, max_length=150, num_beams=4, early_stopping=True)
-    decoded_output = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
-    summary = '.\n'.join(decoded_output.split('. '))
+    summary_ids = model.generate(inputs, max_length=150, num_beams=4, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
     return summary
 
